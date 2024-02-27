46022cc47517483ca1338b700ffacb45c200268d
nico2433 <nicobrab243@gmail.com>
2024-02-26 22:17:00 -0300

    Configured tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+
+const config = {
+  content: ["./src/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
+
+export default config;
