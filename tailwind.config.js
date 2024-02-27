65867ee1bc2ed684554ecdcf474155f6fdbc3e37
nico2433 <nicobrab243@gmail.com>
2024-02-26 22:19:48 -0300

    updated tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 const config = {
-  content: ["./src/*.{js,ts,jsx,tsx,mdx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {},
   },
