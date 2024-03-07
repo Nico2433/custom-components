d5c67a913118265c8ed612191a015d61ecfca7eb
nico2433 <nicobrab243@gmail.com>
2024-03-07 06:17:08 -0300

    Removed ssr on build

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import dts from "vite-plugin-dts";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    ssr: true,
     lib: {
       entry: resolve(__dirname, "./src/index.ts"),
       name: "react-beautiful-timeline",
